fb96e11c3405119a73b0570e91c2bd8a7f586e28
willemodendaal <willem@newmedialabs.co.za>
2017-10-16 11:39:39 +0200

    Fix for bug #61. Null reference error on empty "type" field.

--- ClientLibrary/Structures/LuisResult.cs
+++ ClientLibrary/Structures/LuisResult.cs
@@ -187,20 +187,19 @@ namespace Microsoft.Cognitive.LUIS
             return a;
         }
 
-        /// <summary>
-        /// Parses a json array of entities into an entity dictionary.
-        /// </summary>
-        /// <param name="array"></param>
-        /// <returns>The object containing the dictionary of entities</returns>
-        private IDictionary<string, IList<Entity>> ParseEntityArrayToDictionary(JArray array)
+        private IDictionary<string, IList<Entity>> ParseEntityArrayToDictionary (JArray array)
         {
-            var count = array.Count;
             var dict = new Dictionary<string, IList<Entity>>();
 
             foreach (var item in array)
             {
                 var e = new Entity();
-                e.Load((JObject)item);
+                e.Load((JObject) item);
+
+                if (ShouldIgnoreEntity(e))
+                {
+                    continue;
+                }
 
                 IList<Entity> entityList;
                 if (!dict.TryGetValue(e.Name, out entityList))
@@ -212,8 +211,38 @@ namespace Microsoft.Cognitive.LUIS
                     entityList.Add(e);
                 }
             }
-
             return dict;
+        }
+
+        private bool ShouldIgnoreEntity (Entity e)
+        {
+            //Luis sometimes returns entities with "type" equal to null (mapped to "Name").
+            //  Ignore these kinds of entities.
+            //  For example (note how the first two entities 'add up' to the last one):
+            //    "entities": [
+            //    {
+            //        "entity": "opportunity",
+            //        "type": null,
+            //        "startIndex": 18,
+            //        "endIndex": 28,
+            //        "score": 0.179574952
+            //    },
+            //    {
+            //        "entity": "fund",
+            //        "type": null,
+            //        "startIndex": 30,
+            //        "endIndex": 33,
+            //        "score": 0.164282173
+            //    },
+            //    {
+            //        "entity": "opportunity fund",
+            //        "type": "Fund",
+            //        "startIndex": 18,
+            //        "endIndex": 33,
+            //        "score": 0.996879935
+            //    }
+            //]
+            return e.Name == null;
         }
 
         /// <summary>
